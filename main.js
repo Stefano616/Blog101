be3e323b60e7c6ebd9142134c69883ffc92a2ae9
Stefano Rocchetti <stefanorocchetti4@gmail.com>
2024-03-19 10:35:48 +0100

    Added social Icons in the navbar. Converted logo in anchor element to go back to index.html. Inserted some missing closing tags

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 import { changeSlide, currentSlide } from "./modules/slideshow.js";
 
-const [btnChangeSlidePrev, btnChangeSlideNext] = document.getElementsByClassName("btn-change-slide");
+const [btnChangeSlidePrev, btnChangeSlideNext] =
+  document.getElementsByClassName("btn-change-slide");
 const dotChangeSlideArr = document.getElementsByClassName("dot");
 const categoriesCards = document.getElementsByClassName("card");
 
 for (let i = 0; i < categoriesCards.length; i++) {
-    categoriesCards[i].addEventListener("click", () => {
-        localStorage.setItem(`${i}`, categoriesCards[i].id);
-        console.log(categoriesCards[i].id)
-    })
+  categoriesCards[i].addEventListener("click", () => {
+    localStorage.setItem(`${i}`, categoriesCards[i].id);
+    console.log(categoriesCards[i].id);
+  });
 }
 
 btnChangeSlidePrev.addEventListener("click", () => changeSlide(-1));
@@ -19,4 +20,3 @@ dotChangeSlideArr[1].addEventListener("click", () => currentSlide(2));
 dotChangeSlideArr[2].addEventListener("click", () => currentSlide(3));
 
 setInterval(changeSlide, 3000);
-
